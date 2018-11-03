c461aeebf72da7e010725aab18cb558d977fb26c
Bishoy Boktor <bishoyboktor@Bishoys-MacBook-Pro.local>
2018-11-03 00:58:28 -0500

    Checkpoint Database code

--- Virtual_Graffiti/src/main.rs
+++ Virtual_Graffiti/src/main.rs
@@ -1,8 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
 use rocket::http::RawStr;
+use rocket_contrib::{Json, value};
 
 #[get("/")]
 fn index() -> &'static str {
